da7e728b70e6f51b225e783a0ce0e9d999ba0252
Luke Stevenson <luke@lucanos.com>
2013-07-15 01:57:24 +1000

    Optimised Social Icons
    Collapsed into an array/loop.
    Modified CSS to be more easily extended/styled.
    Created copies of icons (with names keyed to the uid fields) for simpler
    use

--- footer.php
+++ footer.php
@@ -1,137 +1,93 @@
 <?php
 
 // Exit if accessed directly
-if ( !defined('ABSPATH')) exit;
+if( !defined( 'ABSPATH' ) )
+  exit;
 
 /**
- * Footer Template
+ * Header Template
  *
  *
  * @file           footer.php
- * @package        Responsive 
- * @author         Emil Uzelac 
- * @copyright      2003 - 2013 ThemeID
- * @license        license.txt
- * @version        Release: 1.2
- * @filesource     wp-content/themes/responsive/footer.php
- * @link           http://codex.wordpress.org/Theme_Development#Footer_.28footer.php.29
- * @since          available since Release 1.0
+ * @package        Envy Responsive Child
+ * @author         Luke Stevenson <luke@lumik.com.au>
  */
 
-/* 
+/*
  * Globalize Theme options
  */
 global $responsive_options;
 $responsive_options = responsive_get_options();
 ?>
 		<?php responsive_wrapper_bottom(); // after wrapper content hook ?>
-    </div><!-- end of #wrapper -->
-    <?php responsive_wrapper_end(); // after wrapper hook ?>
+  </div><!-- end of #wrapper -->
+  <?php responsive_wrapper_end(); // after wrapper hook ?>
 </div><!-- end of #container -->
 <?php responsive_container_end(); // after container hook ?>
-
 <div id="footer" class="clearfix">
 	<?php responsive_footer_top(); ?>
+  <div id="footer-wrapper">
+    <div class="grid col-940">
+      <div class="grid col-540">
+<?php if( has_nav_menu( 'footer-menu' , 'responsive' ) ){ ?>
+        <?php wp_nav_menu(array(
+                'container'    => '' ,
+                'fallback_cb'	   =>  false ,
+                'menu_class'     => 'footer-menu' ,
+                'theme_location' => 'footer-menu' )); ?>
+<?php } ?>
+      </div><!-- end of col-540 -->
+      <div class="grid col-380 fit">
+        <?php
 
-    <div id="footer-wrapper">
-    
-        <div class="grid col-940">
-        
-        <div class="grid col-540">
-		<?php if (has_nav_menu('footer-menu', 'responsive')) { ?>
-	        <?php wp_nav_menu(array(
-				    'container'       => '',
-					'fallback_cb'	  =>  false,
-					'menu_class'      => 'footer-menu',
-					'theme_location'  => 'footer-menu')
-					); 
-				?>
-         <?php } ?>
-         </div><!-- end of col-540 -->
-         
-         <div class="grid col-380 fit">
-         <?php
-					
-            // First let's check if any of this was set
-		
-                echo '<ul class="social-icons">';
-					
-                if (!empty($responsive_options['twitter_uid'])) echo '<li class="twitter-icon"><a href="' . $responsive_options['twitter_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/twitter-icon.png" width="24" height="24" alt="Twitter">'
-                    .'</a></li>';
+$social_icons = array(
+  'twitter'      => 'Twitter' ,
+  'facebook'     => 'Facebook' ,
+  'linkedin'     => 'LinkedIn' ,
+  'youtube'      => 'YouTube' ,
+  'stumble-upon' => 'StumbleUpon' ,
+  'rss'          => 'RSS Feed' ,
+  'google_plus'  => 'Google Plus' ,
+  'instagram'    => 'Instagram' ,
+  'pinterest'    => 'Pinterest' ,
+  'yelp'         => 'Yelp!' ,
+  'vimeo'        => 'Vimeo' ,
+  'foursquare'   => 'foursquare'
+);
+$social_template = '<li class="%1$s-icon"><a href="%3$s"><img src="' . get_template_directory_uri() . '/core/icons/%1$s-icon.png" width="24" height="24" alt="%2$s"></a></li>';
 
-                if (!empty($responsive_options['facebook_uid'])) echo '<li class="facebook-icon"><a href="' . $responsive_options['facebook_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/facebook-icon.png" width="24" height="24" alt="Facebook">'
-                    .'</a></li>';
-  
-                if (!empty($responsive_options['linkedin_uid'])) echo '<li class="linkedin-icon"><a href="' . $responsive_options['linkedin_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/linkedin-icon.png" width="24" height="24" alt="LinkedIn">'
-                    .'</a></li>';
-					
-                if (!empty($responsive_options['youtube_uid'])) echo '<li class="youtube-icon"><a href="' . $responsive_options['youtube_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/youtube-icon.png" width="24" height="24" alt="YouTube">'
-                    .'</a></li>';
-					
-                if (!empty($responsive_options['stumble_uid'])) echo '<li class="stumble-upon-icon"><a href="' . $responsive_options['stumble_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/stumble-upon-icon.png" width="24" height="24" alt="StumbleUpon">'
-                    .'</a></li>';
-					
-                if (!empty($responsive_options['rss_uid'])) echo '<li class="rss-feed-icon"><a href="' . $responsive_options['rss_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/rss-feed-icon.png" width="24" height="24" alt="RSS Feed">'
-                    .'</a></li>';
-       
-                if (!empty($responsive_options['google_plus_uid'])) echo '<li class="google-plus-icon"><a href="' . $responsive_options['google_plus_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/googleplus-icon.png" width="24" height="24" alt="Google Plus">'
-                    .'</a></li>';
-					
-                if (!empty($responsive_options['instagram_uid'])) echo '<li class="instagram-icon"><a href="' . $responsive_options['instagram_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/instagram-icon.png" width="24" height="24" alt="Instagram">'
-                    .'</a></li>';
-					
-                if (!empty($responsive_options['pinterest_uid'])) echo '<li class="pinterest-icon"><a href="' . $responsive_options['pinterest_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/pinterest-icon.png" width="24" height="24" alt="Pinterest">'
-                    .'</a></li>';
-					
-                if (!empty($responsive_options['yelp_uid'])) echo '<li class="yelp-icon"><a href="' . $responsive_options['yelp_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/yelp-icon.png" width="24" height="24" alt="Yelp!">'
-                    .'</a></li>';
-					
-                if (!empty($responsive_options['vimeo_uid'])) echo '<li class="vimeo-icon"><a href="' . $responsive_options['vimeo_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/vimeo-icon.png" width="24" height="24" alt="Vimeo">'
-                    .'</a></li>';
-					
-                if (!empty($responsive_options['foursquare_uid'])) echo '<li class="foursquare-icon"><a href="' . $responsive_options['foursquare_uid'] . '">'
-                    .'<img src="' . get_stylesheet_directory_uri() . '/core/icons/foursquare-icon.png" width="24" height="24" alt="foursquare">'
-                    .'</a></li>';
-             
-                echo '</ul><!-- end of .social-icons -->';
-         ?>
-         </div><!-- end of col-380 fit -->
-         
-         </div><!-- end of col-940 -->
-         <?php get_sidebar('colophon'); ?>
-                
-        <div class="grid col-300 copyright">
-            <?php esc_attr_e('&copy;', 'responsive'); ?> <?php _e(date('Y')); ?><a href="<?php echo home_url('/') ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
-                <?php bloginfo('name'); ?>
-            </a>
-        </div><!-- end of .copyright -->
-        
-        <div class="grid col-300 scroll-top"><a href="#scroll-top" title="<?php esc_attr_e( 'scroll to top', 'responsive' ); ?>"><?php _e( '&uarr;', 'responsive' ); ?></a></div>
-        
-        <div class="grid col-300 fit powered">
-            <a href="<?php echo esc_url(__('http://themeid.com/responsive-theme/','responsive')); ?>" title="<?php esc_attr_e('Responsive Theme', 'responsive'); ?>">
-                    <?php printf('Responsive Theme'); ?></a>
-            <?php esc_attr_e('powered by', 'responsive'); ?> <a href="<?php echo esc_url(__('http://wordpress.org/','responsive')); ?>" title="<?php esc_attr_e('WordPress', 'responsive'); ?>">
-                    <?php printf('WordPress'); ?></a>
-        </div><!-- end .powered -->
-        
-    </div><!-- end #footer-wrapper -->
-    
+foreach( $social_icons as $k => $v ){
+  if( !empty( $responsive_options[$k.'_uid'] ) ){
+    $social_icons[$k] = sprintf( $social_template , $k , $v , $responsive_options[$k.'_uid'] );
+  }else{
+    unset( $social_icons[$k] );
+  }
+}
+
+if( count( $social_icons ) ){
+  echo '<ul class="social-icons">';
+  echo implode( '' , $social_icons );
+  echo '</ul><!-- end of .social-icons -->';
+}
+
+        ?>
+      </div><!-- end of col-380 fit -->
+    </div><!-- end of col-940 -->
+    <?php get_sidebar('colophon'); ?>
+    <div class="grid col-300 copyright">
+      <?php esc_attr_e('&copy;', 'responsive'); ?> <?php _e(date('Y')); ?>
+      <a href="<?php echo home_url('/') ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>"><?php bloginfo('name'); ?></a>
+    </div><!-- end of .copyright -->
+    <div class="grid col-300 scroll-top"><a href="#scroll-top" title="<?php esc_attr_e( 'scroll to top', 'responsive' ); ?>"><?php _e( '&uarr;', 'responsive' ); ?></a></div>
+    <div class="grid col-300 fit powered">
+      <a href="<?php echo esc_url(__('http://themeid.com/responsive-theme/','responsive')); ?>" title="<?php esc_attr_e('Responsive Theme', 'responsive'); ?>"><?php printf('Responsive Theme'); ?></a>
+      <?php esc_attr_e('powered by', 'responsive'); ?>
+      <a href="<?php echo esc_url(__('http://wordpress.org/','responsive')); ?>" title="<?php esc_attr_e('WordPress', 'responsive'); ?>"><?php printf('WordPress'); ?></a>
+    </div><!-- end .powered -->
+  </div><!-- end #footer-wrapper -->
 	<?php responsive_footer_bottom(); ?>
 </div><!-- end #footer -->
 <?php responsive_footer_after(); ?>
-
 <?php wp_footer(); ?>
 </body>
 </html>
